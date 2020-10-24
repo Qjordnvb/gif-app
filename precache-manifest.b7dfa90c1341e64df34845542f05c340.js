303c67f03cf522b7f2546fbba71f35199fdd1024
unknown <jordanvillarreal1994@gmail.com>
2020-10-24 12:53:05 -0500

    Updates

---
+++ precache-manifest.b7dfa90c1341e64df34845542f05c340.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8acdce6d596ac036ff30e195cb8bdfae",
+    "url": "/gif-app/index.html"
+  },
+  {
+    "revision": "c4f9c4555b24a9c02924",
+    "url": "/gif-app/static/css/3.5eef0e01.chunk.css"
+  },
+  {
+    "revision": "6b60278f549b05261c51",
+    "url": "/gif-app/static/css/main.0a195a1a.chunk.css"
+  },
+  {
+    "revision": "6a832e26f716029fcd02",
+    "url": "/gif-app/static/js/2.988754cc.chunk.js"
+  },
+  {
+    "revision": "216e537bacc64f9dbaabd26649a9ab95",
+    "url": "/gif-app/static/js/2.988754cc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c4f9c4555b24a9c02924",
+    "url": "/gif-app/static/js/3.7c6959a4.chunk.js"
+  },
+  {
+    "revision": "30d44483be6ef6203f71",
+    "url": "/gif-app/static/js/4.807ee469.chunk.js"
+  },
+  {
+    "revision": "9b562e132a321e089288",
+    "url": "/gif-app/static/js/5.23138693.chunk.js"
+  },
+  {
+    "revision": "6b60278f549b05261c51",
+    "url": "/gif-app/static/js/main.231aea8e.chunk.js"
+  },
+  {
+    "revision": "f92046725f9822061eac",
+    "url": "/gif-app/static/js/runtime-main.c8f680e3.js"
+  }
+]);
