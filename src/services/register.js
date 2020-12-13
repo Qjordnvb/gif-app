85c09adbc0ca089154eedc58adbc539abd94806e
unknown <jordanvillarreal1994@gmail.com>
2020-12-13 13:58:24 -0500

    more changes

--- src/services/register.js
+++ src/services/register.js
@@ -1,14 +1,19 @@
-const ENDPOINT = 'https://deno-api-users-login.herokuapp.com'
+const ENDPOINT =
+  "https://gif-app-react.herokuapp.com";
 
-export default function register ({ username, password }) {
+export default function register({
+  username,
+  password,
+}) {
   return fetch(`${ENDPOINT}/register`, {
-    method: 'POST',
+    method: "POST",
     headers: {
-      "Content-Type": "application/json"
+      "Content-Type": "application/json",
     },
-    body: JSON.stringify({username, password})
-  }).then(res => {
-    if (!res.ok) throw new Error('Response is NOT ok')
-    return true
-  })
+    body: JSON.stringify({ username, password }),
+  }).then((res) => {
+    if (!res.ok)
+      throw new Error("Response is NOT ok");
+    return true;
+  });
 }
